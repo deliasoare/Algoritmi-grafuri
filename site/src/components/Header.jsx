91f4bea2a8a3349adb65b3439f00972741d60949
Delia <deliasoare006@yahoo.com>
2024-04-03 09:50:02 +0300

    Adaugare html header

--- site/src/components/Header.jsx
+++ site/src/components/Header.jsx
@@ -3,10 +3,20 @@ import { useState } from 'react';
 function Header() {
     return (
         <div className='header'>
-            <ul className='right-side'>
-                <li>(logo)</li>
-                <li>Descriere</li>
-                <li>Proiecte</li>
+            <ul className='left-side'>
+                <li>
+                    <div className="logo-img">
+                        <img className='logo' src="" alt="" />
+                    </div>
+                </li>
+            </ul>
+            <ul className="right-side">
+                <li>
+                    <button className="desc-button">Descriere</button>
+                </li>
+                <li>
+                    <button className='projects-button'>Proiecte</button>
+                </li>
             </ul>
         </div>
     )
